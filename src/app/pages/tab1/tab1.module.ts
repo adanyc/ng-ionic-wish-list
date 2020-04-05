50078002a350b83a661f1895d67689b9fdf48717
adanyc <adanyc@gmail.com>
2020-04-05 13:07:50 -0500

    first commit

--- src/app/pages/tab1/tab1.module.ts
+++ src/app/pages/tab1/tab1.module.ts
@@ -3,17 +3,17 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { Tab2Page } from './tab2.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { Tab1Page } from './tab1.page';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
-    RouterModule.forChild([{ path: '', component: Tab2Page }])
+    ComponentsModule,
+    RouterModule.forChild([{ path: '', component: Tab1Page }])
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab1Page]
 })
-export class Tab2PageModule {}
+export class Tab1PageModule {}
